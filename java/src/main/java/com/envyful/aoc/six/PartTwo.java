611dfa49747ed7e0f92f5813107c99a3e0207a93
Daniel Norris <dnorris443@gmail.com>
2022-12-06 05:09:29 +0000

    feat(six): part one & two

---
+++ java/src/main/java/com/envyful/aoc/six/PartTwo.java
@@ -0,0 +1,34 @@
+package com.envyful.aoc.six;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import java.util.List;
+import java.util.Set;
+
+public class PartTwo {
+
+    public static int getCodeStart(String text) {
+        char[] chars = text.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (i < 14) {
+                continue;
+            }
+
+            Set<Character> key = Sets.newHashSet();
+            List<Character> builder = Lists.newArrayList();
+
+            for (int a = 0; a < 14; a++) {
+                key.add(chars[i - a]);
+                builder.add(chars[i - a]);
+            }
+
+            if (key.size() == 4) {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+}
